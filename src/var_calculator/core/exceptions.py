915cc08d5c8af704df853a01809cf8001a9536c1
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2025-07-15 22:28:43 +0530

    Update exceptions.py

---
+++ src/var_calculator/core/exceptions.py
@@ -0,0 +1,7 @@
+class DataFetchError(Exception):
+    """Custom exception for data fetching errors"""
+    pass
+
+class CalculationError(Exception):
+    """Custom exception for calculation errors"""
+    pass
