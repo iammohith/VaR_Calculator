9b520c449f7e16f88a2133c719cefc3cc207cc61
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2025-07-15 23:38:20 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ import getpass
 from datetime import datetime
 
 # Base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Data directories
 DATA_DIR = os.path.join(BASE_DIR, 'data')
