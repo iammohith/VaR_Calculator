1d2ce98d469e00767424bf2eeab31e73b2720860
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2025-07-15 16:06:36 +0530

    Create exceptions.py

---
+++ src/core/exceptions.py
@@ -0,0 +1,7 @@
+class DataFetchError(Exception):
+    """Exception raised when data fetching fails"""
+    pass
+
+class InvalidInputError(Exception):
+    """Exception raised for invalid input parameters"""
+    pass
