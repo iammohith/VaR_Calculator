7af75a664bac1c2efdc067a26697bc673b24e596
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2025-07-15 16:10:08 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VaR_Calculator',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'yfinance',
+        'numpy',
+        'pandas',
+        'scipy',
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'VaR-calculator = bin.VaR_calculator:main',
+        ],
+    },
+)
